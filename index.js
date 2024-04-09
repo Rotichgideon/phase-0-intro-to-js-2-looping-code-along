26099bd9c6ef290906cc469805437d7d16e8a133
gideon rotich <gideonrotich2001@gmail.com>
2024-04-09 22:38:38 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  // Export the functions to make them accessible in other files
+  module.exports = {
+    writeCards,
+    countDown,
+  };
+  
